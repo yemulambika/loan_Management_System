58c1f3230efd8c0574b30b2afb0e4a9566f346dd
yemulambika <ambikayemul2001@gmail.com>
2026-05-16 18:26:46 +0530

    Fix routing and cors

--- client/client/services/api.ts
+++ client/client/services/api.ts
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api`,
 });
-
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
 
--- server/src/app.ts
+++ server/src/app.ts
@@ -7,46 +7,35 @@ import paymentRoutes from "./routes/paymentRoutes.js";
 import dashboardRoutes from "./routes/dashboardRoutes.js";
 
 import errorMiddleware from "./middleware/errorMiddleware.js";
-import usersRouter from "./routes/authRoutes.js";
 
 const app = express();
 
-/* Middlewares */
-app.use(cors());
-app.use("/api", usersRouter);
-
-app.use(express.json());
-
 app.use(
-  express.urlencoded({
-    extended: true,
+  cors({
+    origin: "https://loan-management-system-seven-xi.vercel.app",
+    credentials: true,
   })
 );
 
-/* Static Folder */
-app.use(
-  "/uploads",
-  express.static("uploads")
-);
 
-/* Health Route */
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use("/uploads", express.static("uploads"));
+
+
 app.get("/", (req, res) => {
   res.send("Loan Management API Running");
 });
 
-/* Routes */
+
 app.use("/api/auth", authRoutes);
-
 app.use("/api/loan", loanRoutes);
-
 app.use("/api/payment", paymentRoutes);
+app.use("/api/dashboard", dashboardRoutes);
 
-app.use(
-  "/api/dashboard",
-  dashboardRoutes
-);
 
-/* Error Middleware */
 app.use(errorMiddleware);
 
 export default app;
