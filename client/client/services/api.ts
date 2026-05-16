89f870f5da81424bf59c2e885c74a105102e5918
yemulambika <ambikayemul2001@gmail.com>
2026-05-16 17:59:32 +0530

    Port intial to render

--- client/client/services/api.ts
+++ client/client/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
 API.interceptors.request.use((config) => {
